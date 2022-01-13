9160207b16242d542e8266bca1df35be6074a175
dam <dam@203P12>
2022-01-13 10:03:10 +0100

    Tercer Commit

--- src/versiones/Versiones.java
+++ src/versiones/Versiones.java
@@ -21,10 +21,14 @@ public class Versiones {
 }
 
 /**
- * Click derecho en proyecto - Versioning - Initialize Git Repository - Se crea
- * una carpeta oculta en el proyecto. Archivo/clase verde = nuevo sin guardar.
- * Click d. en proyecto - Git - Commit (confirmar version) (Click d. en archivo
- * - Git - Commit (solo el archivo))
+ * Click derecho en proyecto - Versioning - Initialize Git Repository - Se crea una carpeta oculta en el proyecto. Archivo/clase verde = nuevo sin guardar.
+ * Click d. en proyecto - Git - Commit (confirmar version) (Click d. en archivo - Git - Commit (solo el archivo)) 
+ * Comentario sobre versiones en el recuadro
  * Colores: Verde = añadido/Azul=Modificado/Rojo=Eliminado
- * Team - Diff - Diff to Head
+ * Team - Diff - Diff to Head (2 pestañas: Head = Última versión / Modified = Versión actual)
+ * Diff sirve para comparar el último guardado con lo actual
+ * Se puede añadir de la antigua versión a la actual con la flecha, y eliminar de la actual con la x
+ * Diff (Arriba): Donde pone Local Diff, seleccionar "Select" y veremos los commits realizados
+ * En "To:" seleccionar con que se va a compara el seleccionado anteriormente
+ * 
  */
