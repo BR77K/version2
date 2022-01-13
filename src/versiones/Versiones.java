3dadf1393a0ef28ba3954c2b06ac3632da779996
dam <dam@203P12>
2022-01-13 09:52:29 +0100

    Segundo commit

--- src/versiones/Versiones.java
+++ src/versiones/Versiones.java
@@ -8,20 +8,23 @@ package versiones;
  *
  * @author dam
  */
-public class Versiones {
+public class Versiones {    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Segunda linea");
     }
-    
+
 }
 
 /**
- * Click derecho en proyecto - Versioning - Initialize Git Repository - Se crea una carpeta oculta en el proyecto.
- * Archivo/clase verde = nuevo sin guardar.
- * Click d. en proyecto - Git - Commit
- * Click d. en archivo - Git - Commit
+ * Click derecho en proyecto - Versioning - Initialize Git Repository - Se crea
+ * una carpeta oculta en el proyecto. Archivo/clase verde = nuevo sin guardar.
+ * Click d. en proyecto - Git - Commit (confirmar version) (Click d. en archivo
+ * - Git - Commit (solo el archivo))
+ * Colores: Verde = añadido/Azul=Modificado/Rojo=Eliminado
+ * Team - Diff - Diff to Head
  */
