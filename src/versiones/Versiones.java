2df0fc4f73aedbd5d02a2db83a34cba703dcdc82
dam <dam@203P12>
2022-01-13 09:41:40 +0100

    Primeros cambios

---
+++ src/versiones/Versiones.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package versiones;
+
+/**
+ *
+ * @author dam
+ */
+public class Versiones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
+
+/**
+ * Click derecho en proyecto - Versioning - Initialize Git Repository - Se crea una carpeta oculta en el proyecto.
+ * Archivo/clase verde = nuevo sin guardar.
+ * Click d. en proyecto - Git - Commit
+ * Click d. en archivo - Git - Commit
+ */
